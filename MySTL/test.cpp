7c05e1e2954e9957601f4e580b151cf653dad6f3
kakaacat <1912445179@qq.com>
2023-04-04 20:18:46 +0800

    [test]

---
+++ MySTL/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "vector.h"
+
+
+
+int main()
+{
+	std::cout << "----------vector test--------" << std::endl;
+	mystl::vector<int> v1;
+	v1.push_back(1);
+
+
+	return (0);
+}
