f02c985afbaa88a3963d6e1a8b6d8503d733919a
kakaacat <1912445179@qq.com>
2023-04-04 20:31:44 +0800

    [vector_test]

---
+++ MySTL/vector_test.h
@@ -0,0 +1,11 @@
+#ifndef MYSTL_VECTOR_TEST_H
+#define MYSTL_VECTOR_TEST_H
+
+// vector test : 测试 vector 的接口与 push_back 的性能
+
+#include <vector>
+
+#include "vector.h"
+
+#endif // !MYSTL_VECTOR_TEST_H
+
