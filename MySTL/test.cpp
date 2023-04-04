defeb98f42779c20c04b64fa943f443b15295747
kakaacat <1912445179@qq.com>
2023-04-04 20:34:39 +0800

    [vector test]

---
+++ MySTL/test.cpp
@@ -0,0 +1,27 @@
+#ifdef _MSC_VER
+#define _SCL_SECURE_NO_WARNINGS
+#endif
+
+#if defined(_MSC_VER) && defined(_DEBUG)
+#define _CRTDBG_MAP_ALLOC 
+#include <stdlib.h>
+#include <crtdbg.h>
+#endif // check memory leaks
+
+#include "vector_test.h"
+
+
+int main()
+{
+	using namespace mystl::test;
+
+	std::cout.sync_with_stdio(false);
+
+	RUN_ALL_TESTS();
+	vector_test::vector_test();
+
+#if defined(_MSC_VER) && defined(_DEBUG)
+	_CrtDumpMemoryLeaks();
+#endif // check memory leaks
+
+}
